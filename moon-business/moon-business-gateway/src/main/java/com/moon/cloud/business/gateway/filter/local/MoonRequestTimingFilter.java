86b574c719474cff3d4c6e4a0c160b6e4e7678c3
王星磊 <wangxinglei@yonyou.com>
2025-06-24 15:01:36 +0800

    gateway

--- moon-business/moon-business-gateway/src/main/java/com/moon/cloud/business/gateway/filter/local/MoonRequestTimingFilter.java
+++ moon-business/moon-business-gateway/src/main/java/com/moon/cloud/business/gateway/filter/local/MoonRequestTimingFilter.java
@@ -2,10 +2,12 @@ package com.moon.cloud.business.gateway.filter.local;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Order(2)
 @Component
 public class MoonRequestTimingFilter implements GatewayFilter {
 
