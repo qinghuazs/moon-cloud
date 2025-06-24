59f36be0753aad9dc6179f4a5fe8d64d795d03d7
王星磊 <wangxinglei@yonyou.com>
2025-06-24 15:01:22 +0800

    gateway

---
+++ moon-business/moon-business-gateway/src/main/java/com/moon/cloud/business/gateway/MoonCloudGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.moon.cloud.business.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonCloudGatewayApplication.class, args);
+    }
+}
